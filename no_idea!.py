749a0747827c9405f60f6444eefdc54eeea03a5a
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2022-08-13 23:10:35 +0530

    Update no_idea!.py

--- no_idea!.py
+++ no_idea!.py
@@ -26,3 +26,30 @@ for i in inp:
         h-=inp[i]  
 print(h)
 #passed test case 0, 1, 2, 7 (code works but not better than the previous one need to reduce time complexity)
+m=list(map(int,input().split()))
+inp=list(map(int,input().split()))[0:m[0]]
+A=list(map(int,input().split()))[0:m[1]]
+B=list(map(int,input().split()))[0:m[1]]
+happiness=0
+i=0
+l=len(inp)
+while i<l:
+    if inp[i] in A:
+        happiness+=1
+    elif inp[i] in B:
+        happiness-=1
+    i+=1    
+print(happiness)
+#trail four which is successfull
+from collections import Counter
+m=list(map(int,input().split()))
+inp=Counter(list(map(int,input().split()))[0:m[0]])
+A=set(map(int,input().split()))
+B=set(map(int,input().split()))
+h=0
+for i in inp:
+    if i in A:
+        h+=inp[i]
+    elif i in B:
+        h-=inp[i]  
+print(h)
