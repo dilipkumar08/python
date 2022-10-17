cb75ba6e5deb7a8e088a6c36980e4852a1cc265d
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2022-10-17 10:51:10 +0530

    Create checkerpattern_1_2.py
    checkerpattern  array of 1s and 2s with size n

---
+++ checkerpattern_1_2.py
@@ -0,0 +1,33 @@
+#method-1
+import numpy as np
+def checkerboard(n):
+    l=[]
+    li=[]
+    one=1
+    temp=0
+    two=2
+    for i in range(n*n):
+        l.append(one)
+        temp=one
+        one=two
+        two=temp
+        if len(l)==n:
+            li.append(l)
+            l=[]    
+    arr=np.array(li,dtype=int)
+    print(arr)
+    
+if __name__=="__main__":
+    n=int(input("Enter an integer: "))
+    checkerboard(n)
+    
+#method-2
+import numpy as np 
+def checkerboard(n): 
+    arr = np.ones((n, n), dtype = int) 
+    arr[1::2, ::2] = 2
+    arr[::2, 1::2] = 2
+    print(arr)
+if "__main__"==__name__:
+    n=int(input("Enter an integer: "))
+    checkerboard(n)
