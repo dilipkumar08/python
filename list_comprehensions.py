e86dd5975dd63199556e86746e76e44dac81089a
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2022-11-06 23:10:53 +0530

    Create list_comprehensions.py
    #Hackerrank list comprehensions

---
+++ list_comprehensions.py
@@ -0,0 +1,23 @@
+#method-1
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    l=[[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k!=n]
+    print(l)
+    
+#method-2
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    l=[]
+    for i in range(x+1):
+        for j in range(y+1):
+            for k in range(z+1):
+                if i+j+k!=n:
+                    l.append([i,j,k])
+    print(l)
+    
