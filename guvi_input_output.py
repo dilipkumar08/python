959da579bd2997a90cd5e44b48cd7a1ce0bb0c71
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2023-10-08 18:54:39 +0530

    Create guvi_input_output.py
    guvi input output codekata

---
+++ guvi_input_output.py
@@ -0,0 +1,19 @@
+a=input()
+b=''
+for i in a:
+    b+=i+' '
+b=b[:-1]
+print(b)
+
+#--------------------------
+
+a=list(input().split())
+for i in a:
+    print(i)
+#--------------------
+
+s=input()
+a=''
+for i in s:
+   a=a+i+','
+print(a[:-1])
